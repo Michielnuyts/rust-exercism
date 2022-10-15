29d306b412079e1f28e41011165c77e4bfa4dd71
Michiel Nuyts <michiel@loc.tax>
2022-10-15 17:46:22 +0200

    clock: simplify

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,36 +1,35 @@
 use std::fmt::Display;
 
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let hours = hours + (minutes / 60);
-
         Self {
-            hours: hours.rem_euclid(24),
-            minutes: minutes.rem_euclid(60),
+            minutes: minutes + (hours * 60),
         }
     }
 
     pub fn add_minutes(&mut self, minutes: i32) -> &mut Self {
-        let new_mins = self.minutes + minutes;
-        let hours = (new_mins / 60).rem_euclid(24);
-        let mins = new_mins.rem_euclid(60);
-
-        self.hours += hours;
-        self.minutes += mins;
-
+        self.minutes += minutes;
         self
+    }
+
+    fn convert(&self) -> (i32, i32) {
+        let hours = (self.minutes / 60).rem_euclid(24);
+        let minutes = self.minutes.rem_euclid(60);
+
+        (hours, minutes)
     }
 }
 
 impl Display for Clock {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let hours = format!("{:0>2}", self.hours).replace("24", "00");
-        let minutes = format!("{:0>2}", self.minutes);
+        let (hours, minutes) = self.convert();
+
+        let hours = format!("{:0>2}", hours).replace("24", "00");
+        let minutes = format!("{:0>2}", minutes);
 
         write!(f, "{}:{}", hours, minutes)
     }
